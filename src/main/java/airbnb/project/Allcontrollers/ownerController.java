d11b6694ef0287120d9fbeabd9e5469b7856a762
deep jyot <deep jyot@DESKTOP-GQTBUKN>
2025-12-01 11:38:30 +0530

    New Thymeleaf Page Added & changes in owner controller

--- src/main/java/airbnb/project/Allcontrollers/ownerController.java
+++ src/main/java/airbnb/project/Allcontrollers/ownerController.java
@@ -11,6 +11,7 @@ public class ownerController {
     public String owner()
     {
         System.out.println("In Controller");
+        System.out.println("Hello World");
         return "owner";
     }
     
